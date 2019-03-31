0e5c0f2f9618b423eb916ae19da955e8dd210a44
Travis Shen <TravyLockett@gmail.com>
2019-03-31 01:37:02 -0400

    finding all solutions

--- schedule.cc
+++ schedule.cc
@@ -1,20 +1,26 @@
+//copyrights reserved
+//get all solutions
+
+
 #include <stdio.h>
 #include "course.h"
 
 // see if two course have time conflict
 bool goodCourse(course* c1, course* c2){
-	if (c1->sT<c2->sT){
-		if(c1->eT>c2->sT && c1->d==c2->d){
+	if (c1->sT < c2->sT){
+		if(c1->eT > c2->sT && c1->d == c2->d){
 			return false;
 		}else{
 			return true;
 		}
-	}else if(c1->sT>c2->sT){
-		if(c2->eT>c1->sT && c1->d==c2->d){
+	}else if(c1->sT > c2->sT){
+		if(c2->eT > c1->sT && c1->d == c2->d){
 			return false;
 		}else{
 			return true;
 		}
+	}else if(c1->sT == c2->sT && c1->d == c2->d){
+		return false;
 	}
 	
 };
@@ -24,68 +30,60 @@ int main(){
 	int cor=0;
 	int fSec=0;
 	int sSec=0;
-	bool bad=false;
-	int secSize []={1,1,1,1,1,1};
+	int secSize []={2,2,2,2,2,2};
 	bool breakout=false;
 	bool breakout2=false;
-
+	int counter=0;
+//working courses
 	course* s0=new course("C0", 4, 1115, 1240, "MW");
 	course* s1=new course("C1", 3, 1330, 1755, "MW");
 	course* s2=new course("C2", 4, 1830, 2120, "MW");
 	course* s3=new course("C3", 4, 910, 1100, "TR");
 	course* s4=new course("C4", 3, 1245, 1410, "TR");
 	course* s5=new course("C5", 3, 1420, 1535, "TR");
-	
-	course* corArr [6][1]={{s0},{s1},{s2},{s3},{s4},{s5}};
+
+	course* s6=new course("C0", 4, 1115, 1130, "MW");
+	course* s7=new course("C1", 3, 1120, 1755, "MW");
+	course* s8=new course("C2", 4, 1630, 2120, "MW");
+	course* s9=new course("C3", 4, 810, 1000, "TR");
+	course* s10=new course("C4", 3, 945, 1410, "TR");
+	course* s11=new course("C5", 3, 1320, 1535, "TR");
+
+
+	course* corArr [6][2]={{s6,s0},{s7,s1},{s2,s8},{s9,s3},{s10,s4},{s5,s11}};
 
 
 
 	for(cor=0;cor<6;cor++){
-		if(cor==5 && bad != true){
-			printf("good\n");
+		if(cor==5){
+			printf("here is the schedule\n");
 			break;
-		}else if(bad ==true){
-		       printf("Dont break\n");
-		       break;
 		}
  		while(cInc<6-cor){
-			if(breakout==true){
-				break;
-			}
 			while(fSec<=secSize[cor]){
 				if(fSec >= secSize[cor]){
-					bad=true;
-					breakout=true;
-					break;
-				}else if(breakout2==true){
 					break;
 				}
 				while(sSec <= secSize[cor+cInc]-1){
 					if(goodCourse(corArr[cor][fSec], corArr[cor+cInc][sSec])==false){
-						printf("%d,%d and %d,%d are not good \n",cor,fSec,cor+cInc,sSec);
+						counter++;
+						printf("%d,%d and %d,%d are not good %d \n",cor,fSec,cor+cInc,sSec,counter);
 						sSec++;
 					}else if(goodCourse(corArr[cor][fSec], corArr[cor+cInc][sSec])==true){
-						printf("%d,%d and %d,%d are good \n",cor,fSec,cor+cInc,sSec);
-						breakout2=true;
-						sSec=0;
-						break;
+						counter++;
+						printf("%d,%d and %d,%d are good %d \n",cor,fSec,cor+cInc,sSec,counter);
+						sSec++;
 					}
 				}
-				if(breakout2==true){
-					break;
-				}else{
-					fSec++;
-					sSec=0;
-				}
+				fSec++;
+				sSec=0;
 			}
 			fSec=0;
 			cInc++;
-			breakout2=false;
 		}
 		cInc=1;
+
 	}
-
-
 
 }
 	
